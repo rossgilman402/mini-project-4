51c239776123f42b3af0e85ddc41ebb56f94076f
ross402 <65184393+ross402@users.noreply.github.com>
2023-08-25 13:35:40 -0400

    Added index.html, style.css, and script.js

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// DEPENDENCIES
+// start button, reset button, countdown text, win and loss text, word to guess
+
+// DATA
+// wins and losses, word bank, time left
+
+// FUNCTIONS
+
+// USER INTERACTIONS
+// a user types a key ...,clicking buttons
+
+// INITALIZATIONS
+// load wins and losses
